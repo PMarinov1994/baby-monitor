ac87025bdb45d246aaf66d20a712a629f52c2fca
PMarinov1994 <plamen_944@outlook.com>
2025-08-09 00:46:53 +0300

    Update webUI client. Add sound controls.

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,13 +2,24 @@ import { monitorFPS } from "./streamMonitor";
 import { connectToSender } from "./webRTC";
 import { wsConnect } from "./webSocket";
 
-window.addEventListener('DOMContentLoaded', () => {
-    const videoElem = document.getElementById('remoteVideo') as HTMLVideoElement
-    const videoStat = document.getElementById('videoStats') as HTMLParagraphElement
+const videoElem = document.getElementById('remoteVideo') as HTMLVideoElement;
+const videoStat = document.getElementById('videoStats') as HTMLParagraphElement;
 
+const soundSettingsBtn = document.getElementById('soundToggleBtn') as HTMLButtonElement;
+const soundSettingsDiv = document.getElementById('soundSettings') as HTMLDivElement;
+
+
+soundSettingsBtn.addEventListener('click', () => {
+    if (soundSettingsDiv.style.display === 'none')
+        soundSettingsDiv.style.display = 'block';
+    else
+        soundSettingsDiv.style.display = 'none';
+});
+
+window.addEventListener('DOMContentLoaded', () => {
     connectToSender(videoElem).then(() => {
         console.log("Successfull connection")
         monitorFPS(videoElem, videoStat)
-    })
-    wsConnect()
+    });
+    wsConnect();
 });
