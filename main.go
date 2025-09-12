9821d5cdb2a5f355a1ce65f22ccd87d539347d3f
PMarinov1994 <plamen_944@outlook.com>
2025-09-12 20:28:53 +0300

    Buffer a bit video feed

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ const (
 )
 
 var (
-	videoFrames *ringBuffer[[]byte] = createRingBuffer[[]byte](1)
+	videoFrames *ringBuffer[[]byte] = createRingBuffer[[]byte](4)
 	audioFrames *ringBuffer[[]byte] = createRingBuffer[[]byte](1)
 
 	conClients uint8       = 0
